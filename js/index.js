d871383f57f8bb5f449c25dceadfd678991a40d7
Alvin <alvin.mantovani@virtualspirit.me>
2023-11-25 19:57:53 +0700

    feat: handle click and put render mark on the board function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let boardSize = 3;
 let board = [];
+let currentPlayer = 'X';
 
 function initializeBoard() {
     const boardElement = document.getElementById('board');
@@ -15,11 +16,30 @@ function initializeBoard() {
             cell.className = 'cell';
             cell.dataset.row = i;
             cell.dataset.col = j;
+            cell.addEventListener('click', () => handleCellClick(i, j));
             boardElement.appendChild(cell);
         }
     }
 }
 
+function renderBoard() {
+    const cells = document.getElementsByClassName('cell');
 
+    for (let i = 0; i < cells.length; i++) {
+        const row = cells[i].dataset.row;
+        const col = cells[i].dataset.col;
+
+        cells[i].textContent = board[row][col] || '';
+        cells[i].classList.remove('winner');
+    }
+}
+
+function handleCellClick(row, col) {
+    if (!board[row][col]) {
+        board[row][col] = currentPlayer;
+        renderBoard();
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+}
 
 initializeBoard();
